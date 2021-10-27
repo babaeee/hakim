310569a239daaecf29af268b170824d24ced7d2d
hkalbasi <hamidrezakalbasi@protonmail.com>
2021-10-27 23:30:50 +0330

    better name for intros

--- hakim-engine/src/engine/tactic/intros.rs
+++ hakim-engine/src/engine/tactic/intros.rs
@@ -22,7 +22,7 @@ pub fn intros(mut frame: Frame, args: impl Iterator<Item = String>) -> Result<Ve
     let mut args = args.peekable();
     if args.peek().is_none() {
         while let Term::Forall(Abstraction { var_ty, body }) = frame.goal.clone().as_ref() {
-            let name = frame.engine.generate_name("x");
+            let name = frame.engine.generate_name("H");
             frame.add_hyp_with_name(&name, var_ty.clone())?;
             frame.goal = subst(body.clone(), term_ref!(axiom name, var_ty));
         }
