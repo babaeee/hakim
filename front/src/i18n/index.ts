85f19010f21d86eed5d31c9f93017353adcc953c
hkalbasi <hamidrezakalbasi@protonmail.com>
2021-11-06 12:24:29 +0330

    pick i18n

---
+++ front/src/i18n/index.ts
@@ -0,0 +1,37 @@
+import enDict from "./en.yml";
+import faDict from "./fa.yml";
+
+let dict = {};
+
+export const getLang = () => {
+  return localStorage.getItem('lang') || 'en';
+};
+
+export const changeLang = () => {
+  const to = getLang() === 'fa' ? 'en' : 'fa';
+  localStorage.setItem('lang', to);
+  window.location.reload();
+};
+
+export const isRTL = () => {
+  return getLang() === 'fa';
+};
+
+export const init = () => {
+  const lang = getLang();
+  if (lang === 'fa') dict = faDict;
+  if (lang === 'en') dict = enDict;
+};
+
+export const getText = (id) => {
+  return dict[id] || id;
+};
+
+export const g = (c, ...v) => {
+  const t = c.map((x) => x.split(' ').map(getText).join(' '));
+  v.push('');
+  const r = t.map((x, i) => x + v[i]).join('');
+  return r;
+};
+
+init();
