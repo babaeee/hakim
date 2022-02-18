f8b2ac5276949a54b4af68cfc19374c4876ac7da
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-02-19 02:01:43 +0330

    use arena allocator in ring

--- hakim-engine/src/interactive/tactic/ring.rs
+++ hakim-engine/src/interactive/tactic/ring.rs
@@ -1,52 +1,54 @@
 use super::{rewrite::get_eq_params, Error::*, Result};
 use crate::{brain::Term, interactive::Frame, TermRef};
+use typed_arena::Arena;
 
 #[derive(Debug, Clone)]
-enum ArithTree {
+enum ArithTree<'a> {
     Atom(TermRef),
     Const(BigInt),
-    Plus(Box<ArithTree>, Box<ArithTree>),
-    Mult(Box<ArithTree>, Box<ArithTree>),
+    Plus(&'a ArithTree<'a>, &'a ArithTree<'a>),
+    Mult(&'a ArithTree<'a>, &'a ArithTree<'a>),
 }
 
 use num_bigint::BigInt;
 use ArithTree::*;
 
 type Poly = (BigInt, Vec<(BigInt, Vec<TermRef>)>);
+type ArithArena<'a> = &'a Arena<ArithTree<'a>>;
 
-fn normalize(tree: ArithTree) -> ArithTree {
+fn normalize<'a>(tree: &'a ArithTree<'a>, arena: ArithArena<'a>) -> &'a ArithTree<'a> {
     match tree {
         Atom(_) | Const(_) => tree,
-        Plus(a, b) => Plus(Box::new(normalize(*a)), Box::new(normalize(*b))),
+        Plus(a, b) => arena.alloc(Plus(normalize(*a, arena), normalize(*b, arena))),
         Mult(a, b) => {
-            let a = normalize(*a);
-            let b = normalize(*b);
+            let a = normalize(*a, arena);
+            let b = normalize(*b, arena);
             if let Plus(x, y) = a {
-                let xb = normalize(Mult(x, Box::new(b.clone())));
-                let yb = normalize(Mult(y, Box::new(b.clone())));
-                return Plus(Box::new(xb), Box::new(yb));
+                let xb = normalize(arena.alloc(Mult(x, b)), arena);
+                let yb = normalize(arena.alloc(Mult(y, b)), arena);
+                return arena.alloc(Plus(xb, yb));
             }
             if let Plus(x, y) = b {
-                let xa = normalize(Mult(x, Box::new(a.clone())));
-                let ya = normalize(Mult(y, Box::new(a.clone())));
-                return Plus(Box::new(xa), Box::new(ya));
+                let xa = normalize(arena.alloc(Mult(x, a)), arena);
+                let ya = normalize(arena.alloc(Mult(y, a)), arena);
+                return arena.alloc(Plus(xa, ya));
             }
-            Mult(Box::new(a), Box::new(b))
+            arena.alloc(Mult(a, b))
         }
     }
 }
 
-fn tree_to_d2(tree: ArithTree) -> Poly {
+fn tree_to_d2(tree: &ArithTree<'_>) -> Poly {
     match tree {
-        Atom(t) => (0.into(), vec![(1.into(), vec![t])]),
-        Const(i) => (i, vec![]),
+        Atom(t) => (0.into(), vec![(1.into(), vec![t.clone()])]),
+        Const(i) => (i.clone(), vec![]),
         Plus(t1, t2) => {
             let (c1, r1) = tree_to_d2(*t1);
             let (c2, r2) = tree_to_d2(*t2);
             (c1 + c2, [r1, r2].concat())
         }
         Mult(t1, t2) => {
-            fn to_mul(x: ArithTree) -> (BigInt, Vec<TermRef>) {
+            fn to_mul(x: &ArithTree<'_>) -> (BigInt, Vec<TermRef>) {
                 let (c1, mut r1) = tree_to_d2(x);
                 if r1.is_empty() {
                     return (c1, vec![]);
@@ -56,35 +58,40 @@ fn tree_to_d2(tree: ArithTree) -> Poly {
                 }
                 r1.pop().unwrap()
             }
-            let (c1, r1) = to_mul(*t1);
-            let (c2, r2) = to_mul(*t2);
+            let (c1, r1) = to_mul(t1);
+            let (c2, r2) = to_mul(t2);
             (0.into(), vec![(c1 * c2, [r1, r2].concat())])
         }
     }
 }
 
-impl From<TermRef> for ArithTree {
-    fn from(t: TermRef) -> Self {
-        match t.as_ref() {
-            Term::App { func, op: op2 } => match func.as_ref() {
-                Term::App { func, op: op1 } => match func.as_ref() {
-                    Term::Axiom { unique_name, .. } => {
-                        if unique_name == "plus" {
-                            Plus(Box::new(op1.clone().into()), Box::new(op2.clone().into()))
-                        } else if unique_name == "mult" {
-                            Mult(Box::new(op1.clone().into()), Box::new(op2.clone().into()))
-                        } else {
-                            Atom(t)
-                        }
+fn term_ref_to_arith(t: TermRef, arena: ArithArena<'_>) -> &ArithTree<'_> {
+    let arith_tree = match t.as_ref() {
+        Term::App { func, op: op2 } => match func.as_ref() {
+            Term::App { func, op: op1 } => match func.as_ref() {
+                Term::Axiom { unique_name, .. } => {
+                    if unique_name == "plus" {
+                        Plus(
+                            term_ref_to_arith(op1.clone(), arena),
+                            term_ref_to_arith(op2.clone(), arena),
+                        )
+                    } else if unique_name == "mult" {
+                        Mult(
+                            term_ref_to_arith(op1.clone(), arena),
+                            term_ref_to_arith(op2.clone(), arena),
+                        )
+                    } else {
+                        Atom(t)
                     }
-                    _ => Atom(t),
-                },
+                }
                 _ => Atom(t),
             },
-            Term::Number { value } => Const(value.clone()),
             _ => Atom(t),
-        }
-    }
+        },
+        Term::Number { value } => Const(value.clone()),
+        _ => Atom(t),
+    };
+    arena.alloc(arith_tree)
 }
 
 fn sorter(x: Poly) -> Poly {
@@ -112,8 +119,8 @@ fn sorter(x: Poly) -> Poly {
     (c, ss)
 }
 
-fn canonical(x: ArithTree) -> Poly {
-    let x = normalize(x);
+fn canonical<'a>(x: &'a ArithTree<'a>, arena: ArithArena<'a>) -> Poly {
+    let x = normalize(x, arena);
     let x = tree_to_d2(x);
 
     sorter(x)
@@ -122,8 +129,9 @@ fn canonical(x: ArithTree) -> Poly {
 pub fn ring(frame: Frame) -> Result<Vec<Frame>> {
     let goal = frame.goal;
     let [op1, op2, _] = get_eq_params(&goal).ok_or(BadGoal("ring only work on equality"))?;
-    let d1 = canonical(ArithTree::from(op1));
-    let d2 = canonical(ArithTree::from(op2));
+    let arena = &Arena::with_capacity(32);
+    let d1 = canonical(term_ref_to_arith(op1, arena), arena);
+    let d2 = canonical(term_ref_to_arith(op2, arena), arena);
     if d1 == d2 {
         Ok(vec![])
     } else {
