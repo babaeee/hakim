4a2e131e8370fb2cd06356bdc45613cac840697b
arshiamoeini <arshiamoeini82@gmail.com>
2022-03-01 16:13:24 +0330

    some lemma

--- hakim-engine/src/library/NumberTheory.v
+++ hakim-engine/src/library/NumberTheory.v
@@ -1,4 +1,5 @@
-Axiom divide_intro: ∀ a b: ℤ, a | b -> ∃ c: ℤ, a * c = b.
+Axiom divide_introl: ∀ a b: ℤ, a | b -> ∃ c: ℤ, a * c = b.
+Axiom divide_intror: ∀ a b: ℤ, ∃ c: ℤ, a * c = b -> a | b.
 Axiom divide_refl: ∀ a: ℤ, a | a.
 Axiom divide_trans: ∀ a b c: ℤ, a | b -> b | c -> a | c.
 Axiom divide_factor: ∀ a b c: ℤ, a | b -> a | b * c.
@@ -10,4 +11,4 @@ Axiom prime_intro_l: ∀ x: ℤ, prime x -> 0 < x ∧ (∀ y: ℤ, 0 < y -> y | 
 Axiom prime_intro_r: ∀ x: ℤ, 0 < x ∧ (∀ y: ℤ, 0 < y -> y | x -> y = 0 ∨ y = x) -> prime x.
 Axiom prime_gt_2: ∀ x: ℤ, prime x -> 2 < x.
 
-Axiom prime_divisor: ∀ x: ℤ, (x = 0 -> False) -> ∃ p: ℤ, p | x.
+Axiom prime_divisor: ∀ x: ℤ, (x = 0 -> False) -> ∃ p: ℤ, prime p ∧ p | x.
