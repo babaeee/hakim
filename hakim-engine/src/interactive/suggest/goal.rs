1d85e9c8f826f6a5fa4582d9550b5621e5c26ec6
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-05-06 19:09:16 +0430

    extract goal to its own module

---
+++ hakim-engine/src/interactive/suggest/goal.rs
@@ -0,0 +1,120 @@
+use crate::brain::Term;
+use crate::interactive::Frame;
+
+use super::SuggClass::*;
+use super::SuggRule;
+use super::Suggestion;
+
+const GOAL_RULES: &[SuggRule] = &[
+    SuggRule {
+        class: Destruct,
+        tactic: &["apply and_intro"],
+        questions: &[],
+        is_default: true,
+    },
+    SuggRule {
+        class: Pattern("A ∨ B", "~ B ⊢ A"),
+        tactic: &["apply or_to_imply", "intros"],
+        questions: &[],
+        is_default: true,
+    },
+    SuggRule {
+        class: Pattern("A ∨ B", "~ A ⊢ B"),
+        tactic: &["apply or_sym", "apply or_to_imply", "intros"],
+        questions: &[],
+        is_default: false,
+    },
+    SuggRule {
+        class: Pattern("A = B", "A ⊆ B ∧ B ⊆ A"),
+        tactic: &["apply set_equality"],
+        questions: &[],
+        is_default: true,
+    },
+    SuggRule {
+        class: Pattern("a ∈ x ∪ y", "a ∈ x ∨ a ∈ y"),
+        tactic: &["apply union_fold"],
+        questions: &[],
+        is_default: true,
+    },
+    SuggRule {
+        class: Pattern("a ∈ {b}", "a = b"),
+        tactic: &["apply singleton_fold"],
+        questions: &[],
+        is_default: true,
+    },
+    SuggRule {
+        class: Pattern("a ∈ {b | f b}", "f a"),
+        tactic: &["apply set_from_func_fold"],
+        questions: &[],
+        is_default: true,
+    },
+    SuggRule {
+        class: Pattern("a ⊆ b", "∀ x: T, x ∈ a -> x ∈ b"),
+        tactic: &["apply included_fold"],
+        questions: &[],
+        is_default: true,
+    },
+    SuggRule {
+        class: Pattern("A ∨ B", "A"),
+        tactic: &["apply or_introl"],
+        questions: &[],
+        is_default: false,
+    },
+    SuggRule {
+        class: Pattern("A ∨ B", "B"),
+        tactic: &["apply or_intror"],
+        questions: &[],
+        is_default: false,
+    },
+    SuggRule {
+        class: Pattern("A ∧ B", "A, B"),
+        tactic: &["apply and_intro"],
+        questions: &[],
+        is_default: true,
+    },
+];
+
+pub fn suggest_on_goal(goal: &Term, frame: &Frame) -> Vec<Suggestion> {
+    let mut r = vec![];
+    for rule in GOAL_RULES {
+        if let Some(x) = rule.try_on_goal(frame.clone()) {
+            r.push(x);
+        }
+    }
+    match goal {
+        Term::Forall(_) => {
+            r.push(Suggestion::new_default(Intros, "intros"));
+            r.push(Suggestion {
+                class: IntrosWithName,
+                tactic: vec!["intros $0".to_string()],
+                questions: vec!["$enter_a_name:".to_string()],
+                is_default: false,
+            });
+        }
+        Term::App { func, op: _ } => {
+            if let Term::App { func, op: _ } = func.as_ref() {
+                if let Term::Axiom { unique_name, .. } = func.as_ref() {
+                    if unique_name.as_str() == "ex" {
+                        r.push(Suggestion::newq1default(
+                            Destruct,
+                            "apply (ex_intro ? ? ($0))",
+                            "$enter_value_that_satisfy:",
+                        ))
+                    };
+                }
+            }
+        }
+        _ => {}
+    }
+    r
+}
+
+pub fn suggest_on_goal_dblclk(goal: &Term, frame: &Frame) -> Option<Suggestion> {
+    let suggs = suggest_on_goal(goal, frame);
+    for sugg in suggs {
+        if sugg.is_default {
+            return Some(sugg);
+        }
+    }
+    None
+}
