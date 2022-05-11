bf5684abd2a4eb00f52038abbd31beb6c48866a8
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-05-12 01:36:44 +0430

    improve induction

--- hakim-engine/src/library/Induction.v
+++ hakim-engine/src/library/Induction.v
@@ -1,1 +1,1 @@
-Axiom simple_induction: ∀ k: ℤ, ∀ P: ℤ -> U, P k -> (∀ n: ℤ, k < n + 1 -> P n -> P (n+1)) -> (∀ n: ℤ, k < n + 1 -> P n).
+Axiom simple_induction: ∀ k: ℤ, ∀ P: ℤ -> U, P k -> (∀ n: ℤ, k ≤ n -> P n -> P (n+1)) -> (∀ n: ℤ, k ≤ n -> P n).
