af2725322600676d33babd308c4920577af1489a
arshiamoeini <arshiamoeini82@gmail.com>
2022-12-06 22:31:46 +0330

    ter

--- hakim-engine/src/interactive/tactic/assumption.rs
+++ hakim-engine/src/interactive/tactic/assumption.rs
@@ -3,7 +3,11 @@
 */
 use super::Result;
 use crate::{
-    analysis::logic::{LogicArena, LogicBuilder, LogicValue},
+    analysis::{
+        arith::ConstRepr,
+        big_fraction::BigFraction,
+        logic::{LogicArena, LogicBuilder, LogicValue},
+    },
     app_ref,
     brain::{Term, TermRef},
     interactive::Frame,
@@ -21,32 +25,32 @@ fn convert(
     term: TermRef,
     _logic_arena: LogicArena<'_, PropStatement>,
 ) -> LogicValue<'_, PropStatement> {
-    /*  if let Term::App { func, op } = term.as_ref() {
-        if let Term::Axiom {unique_name, .. } = func.as_ref() {
+    if let Term::App { func, op } = term.as_ref() {
+        if let Term::Axiom { unique_name, .. } = func.as_ref() {
             if unique_name == "is_q" {
+                if BigFraction::from_term(op).is_some() {
+                    return LogicValue::True;
+                }
                 if let Term::App { func, op: op2 } = op.as_ref() {
                     if let Term::App { func, op: op1 } = func.as_ref() {
                         if let Term::App { func, op } = func.as_ref() {
                             if let Term::Axiom { unique_name, .. } = func.as_ref() {
                                 match unique_name.as_str() {
-                                    "plus" | "minus" => convert(is_q_term(op1), logic_arena).and(convert(is_q_term(op2), logic_arena), arena);
-
-                                "mult" if detect_z_ty(op) || detect_r_ty(op) => Mult(
-                                    term_ref_to_arith(op1.clone(), arena),
-                                    term_ref_to_arith(op2.clone(), arena),
-                                ),
-                                "div" if detect_z_ty(op) || detect_r_ty(op) => Div(
-                                    term_ref_to_arith(op1.clone(), arena),
-                                    term_ref_to_arith(op2.clone(), arena),
-                                ),
-                                _ => atom_normalizer(t),
-                            },
-                            _ => atom_normalizer(t),
-                        },
-
+                                    "plus" | "minus" | "mult" => {
+                                        return convert(is_q_term(op1.clone()), _logic_arena).and(
+                                            convert(is_q_term(op2.clone()), _logic_arena),
+                                            _logic_arena,
+                                        );
+                                    }
+                                    _ => (),
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-    }*/
+    }
     LogicValue::from(PropStatement::Atom(term))
 }
 fn is_q_term(r: TermRef) -> TermRef {
@@ -108,5 +112,10 @@ mod tests {
     #[test]
     fn chars() {
         success("~ 'r' = 'u'");
+    }
+    #[test]
+    fn is_q_checks() {
+        success("is_q 2.");
+        success("∀ x y z t, is_q x -> is_q y -> is_q z -> is_q t -> is_q (x * y + z - t)");
     }
 }
