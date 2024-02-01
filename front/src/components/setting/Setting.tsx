31cb04fc7d84d495aa810f24991b618ef89f6b89
hkalbasi <hamidrezakalbasi@protonmail.com>
2024-02-01 17:54:15 +0330

    Add button to disable auto filter

--- front/src/components/setting/Setting.tsx
+++ front/src/components/setting/Setting.tsx
@@ -1,4 +1,5 @@
 import { getDevState, setDevState, useDevState } from "../../dev_mode";
+import { getAutoProofState, setAutoProofState, useAutoProofState } from "../../dev_mode/auto_proof_state";
 import { changeLang, g, getLang } from "../../i18n";
 
 export const Setting = () => {
@@ -9,11 +10,20 @@ export const Setting = () => {
             setDevState('off');
         }
     };
+    const changeAutoProofMode = () => {
+        if (getAutoProofState()) {
+            setAutoProofState(false);
+        } else {
+            setAutoProofState(true);
+        }
+    };
     const devState = useDevState();
+    const autoState = useAutoProofState();
     return <div style={{ color: 'white', width: '100%' }} dir="ltr">
         <ul>
             <li>{g`change_lang`} <button onClick={changeLang}>{getLang()}</button></li>
             <li>{g`dev_mode`} <button onClick={changeDevMode}>{devState === 'off' ? 'off' : 'on'}</button></li>
+            <li>{g`auto_proof`} <button onClick={changeAutoProofMode}>{!autoState ? 'off' : 'on'}</button></li>
         </ul>
     </div>;
 };
